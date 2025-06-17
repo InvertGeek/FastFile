932dbcb1f64dd7f7bc3ee2d73c21ab8bed3d7944
1 <386029724@qq.com>
2025-06-17 14:10:34 +0800

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,93 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.compose.compiler)
+    id("com.google.gms.google-services")
+    id("kotlin-parcelize")
+}
+
+android {
+    namespace = "com.donut.fastfile"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.donut.fastfile"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 6
+        versionName = "1.2.3.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    applicationVariants.all {
+        val variant = this
+        variant.outputs
+            .map { it as com.android.build.gradle.internal.api.BaseVariantOutputImpl }
+            .forEach { output ->
+                val outputFileName =
+                    "FastFile-${variant.baseName}-${variant.versionName}.apk"
+                output.outputFileName = outputFileName
+            }
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    implementation(files("libs/server.aar"))
+    implementation(libs.fastjson2.kotlin)
+    implementation(libs.kotlin.stdlib)
+    implementation(libs.kotlin.reflect)
+    implementation(libs.mmkv)
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.okhttp)
+    implementation(libs.firebase.analytics)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.network)
+    implementation(libs.ktor.serialization.gson)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.appcompat)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+}
---
+++ app/src/main/java/com/donut/fastfile/App.kt
@@ -0,0 +1,57 @@
+package com.donut.fastfile
+
+
+import android.app.Application
+import android.os.Looper
+import com.donut.fastfile.util.loopTask
+import com.donut.fastfile.util.objects.MixActivity
+import com.donut.fastfile.util.showError
+import com.donut.fastfile.util.showErrorDialog
+import com.tencent.mmkv.MMKV
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+import mobile.Mobile
+
+
+val appScope = CoroutineScope(Dispatchers.Main + SupervisorJob())
+
+lateinit var kv: MMKV
+
+private lateinit var innerApp: Application
+
+
+val currentActivity: MixActivity
+    get() {
+        return MixActivity.firstActiveActivity()!!
+    }
+
+val app: Application
+    get() = innerApp
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Thread.setDefaultUncaughtExceptionHandler { t, e ->
+            showError(e)
+            if (Looper.myLooper() == null) {
+                return@setDefaultUncaughtExceptionHandler
+            }
+            showErrorDialog(e)
+        }
+        innerApp = this
+        MMKV.initialize(this)
+        kv = MMKV.defaultMMKV()
+        appScope.launch(Dispatchers.IO) {
+            Mobile.startServer()
+        }
+        appScope.loopTask(1000 * 60 * 10) {
+            kv.clearMemoryCache()
+            kv.trim()
+        }
+    }
+
+}
+
