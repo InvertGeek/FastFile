0bc6a00f22a4b674fb0f352cfdbf5444e1acc42c
1 <386029724@qq.com>
2025-07-26 12:40:22 +0800

    Update CachedMutableValue.kt

--- app/src/main/java/com/donut/fastfile/util/CachedMutableValue.kt
+++ app/src/main/java/com/donut/fastfile/util/CachedMutableValue.kt
@@ -70,7 +70,9 @@ abstract class CachedMutableValue<T>(
     @Volatile
     private var value: T,
 ) {
+    @Volatile
     private var loaded = false
+    private val lock = Any()
     private val mutex = Mutex()
 
     private var stateValue by mutableLongStateOf(0)
@@ -80,7 +82,7 @@ abstract class CachedMutableValue<T>(
     abstract fun writeCachedValue(value: T)
 
     operator fun getValue(thisRef: Any?, property: Any?): T {
-        synchronized(this) {
+        synchronized(lock) {
             if (!loaded) {
                 value = readCachedValue()
                 loaded = true
@@ -92,7 +94,7 @@ abstract class CachedMutableValue<T>(
 
 
     operator fun setValue(thisRef: Any?, property: Any?, value: T) {
-        synchronized(this) {
+        synchronized(lock) {
             if (this.value == value) {
                 return
             }
