d897a2edfd1e0607fd5e4a106f5bbeb34450ae50
1 <386029724@qq.com>
2025-11-05 12:42:32 +0800

    1.7.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.donut.fastfile"
         minSdk = 26
         targetSdk = 37
-        versionCode = 12
-        versionName = "1.6.3.0"
+        versionCode = 13
+        versionName = "1.7.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
